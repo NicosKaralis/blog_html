3bf105501c97fe4da45c6dcd1ed18655a7b70858
Nicos Karalis <dymytrhy@gmail.com>
2011-06-25 09:26:14 -0300

    Added squeleton rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogHtml::Application.config.secret_token = '9f9d241a74c38948c565d723427c0abd247cca17ab6d48c4307958a68d59315b34e7f2580699ceaa6e8ec7be824db7a95b20606d8872574cc9173d69eee9a40e'
